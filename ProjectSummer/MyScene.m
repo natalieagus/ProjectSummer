66f4e0ecf04ffcdc3a73c98824a05fe59b639a97
natalieagus <nn88blue@gmail.com>
2014-07-22 12:14:57 +0800

    git commit
    committing

--- ProjectSummer/MyScene.m
+++ ProjectSummer/MyScene.m
@@ -8,7 +8,12 @@
 
 #import "MyScene.h"
 
-@implementation MyScene
+@implementation MyScene{
+    UIScrollView* pageScroller;
+    UITapGestureRecognizer *singleTap;
+    
+    
+}
 
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
@@ -16,25 +21,32 @@
         
         self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
         
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"IowanOldStyle-Roman"];
         
         myLabel.text = @"Hello, World!";
+        myLabel.fontColor = [UIColor blackColor];
         myLabel.fontSize = 30;
         myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
                                        CGRectGetMidY(self.frame));
         
         [self addChild:myLabel];
+        //Copying label
+        SKLabelNode *myLabel2 = [myLabel copy];
+        myLabel2.text = @"hehehe";
+        myLabel2.position = CGPointMake(100, 100);
+        [self addChild:myLabel2];
     }
     return self;
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
-    
-    for (UITouch *touch in touches) {
+    NSLog(@"TOuch!");
+    /*for (UITouch *touch in touches) {
         CGPoint location = [touch locationInNode:self];
         
         SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
+        
         
         sprite.position = location;
         
@@ -43,11 +55,53 @@
         [sprite runAction:[SKAction repeatActionForever:action]];
         
         [self addChild:sprite];
-    }
+    }*/
 }
 
 -(void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
+}
+
+-(void)didMoveToView:(SKView *)view{
+    //add gesture recognizer, with the method singletapgesturecaptured, selector identifies the method that is called whenever the tap is captured
+    singleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(singleTapGestureCaptured:)];
+    
+    pageScroller = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height-50)];
+    // Declare the size of the content that will be inside the scroll view
+    // This will let the system know how much they can scroll inside
+    pageScroller.contentSize = CGSizeMake(self.frame.size.width, 1000);
+    
+    UILabel *text1 = [ [UILabel alloc ] initWithFrame:CGRectMake(20, 20, self.frame.size.width-40, 30) ];
+    text1.textAlignment = NSTextAlignmentLeft;
+    text1.font = [UIFont fontWithName:@"Arial" size:(14.0)];
+    text1.text = @"Sample element 1";
+    text1.textColor = [UIColor whiteColor];
+    [pageScroller addSubview:text1];
+    
+    UILabel *text2 = [ [UILabel alloc ] initWithFrame:CGRectMake(20, 500, self.frame.size.width-40, 30) ];
+    text2.textAlignment = NSTextAlignmentLeft;
+    text2.font = [UIFont fontWithName:@"Arial" size:(14.0)];
+    text2.text = @"Sample element 2";
+    text2.textColor = [UIColor whiteColor];
+    [pageScroller addSubview:text2];
+    [self.scene.view addSubview:pageScroller];
+    
+    [pageScroller addGestureRecognizer:singleTap];
+    
+    
+}
+
+
+-(void)singleTapGestureCaptured:(UITapGestureRecognizer*)gesture{
+    CGPoint touchPoint=[gesture locationInView:pageScroller];
+    NSLog(@"YAY!!!");
+    printf("\n %f x %f y \n", touchPoint.x, touchPoint.y);
+}
+
+- (void)willMoveFromView:(SKView *)view
+{
+    [super willMoveFromView:view];
+    [pageScroller removeFromSuperview];
 }
 
 @end
